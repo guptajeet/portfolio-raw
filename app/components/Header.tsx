c611a93a57097624113e13e848f05efc6b49069b
Ajeet Gupta <49100602+guptajeet@users.noreply.github.com>
2025-01-15 14:43:20 +0530

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header() {
       <div className="container flex h-14 items-center">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <span className="hidden font-bold sm:inline-block">Your Name</span>
+            <span className="hidden font-bold sm:inline-block">Ajeet Gupta</span>
           </Link>
           <nav className="flex items-center space-x-6 text-sm font-medium">
             {navItems.map((item) => (
